d3502222da06849ac186d75b8c80fb3ef3e543c4
CodeBrained <cristianking128@gmail.com>
2023-05-21 20:04:28 -0400

    Added a working file reader

--- CEN3024C/module-2/src/module2/Main.java
+++ CEN3024C/module-2/src/module2/Main.java
@@ -1,10 +1,26 @@
 package module2;
+import java.io.*;
 
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.print("Test");
+	public static void main(String[] args) throws Exception {
+		String text = getText();
+		System.out.print(text);
 
+	}
+	
+	//reads the file and converts it to a string
+	public static String getText() throws Exception {
+		String text = new String();
+		FileReader reader = new FileReader("TheRavenPoemWithHTMLTags.txt");
+		
+		int i;
+		while ((i = reader.read()) != -1) {
+			text += (char)i;
+		}
+		
+		reader.close();
+		return text;
 	}
 
 }
