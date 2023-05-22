f197196272334c000bace42cdeaea4da4ac6a19a
CodeBrained <cristianking128@gmail.com>
2023-05-22 01:11:14 -0400

    Split the poem into an array of words

--- CEN3024C/module-2/src/module2/Main.java
+++ CEN3024C/module-2/src/module2/Main.java
@@ -1,26 +1,60 @@
 package module2;
 import java.io.*;
+import org.jsoup.*;
+import org.jsoup.nodes.*;
+import org.jsoup.select.Elements;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		String text = getText();
-		System.out.print(text);
+		//getText();
+		String poemString = getText();
 
+		poemString = poemString.replaceAll("[,]", " ");
+		poemString = poemString.replaceAll("[.]", " ");
+		poemString = poemString.replaceAll("[-]", " ");
+		poemString = poemString.replaceAll("[—]", " ");
+		poemString = poemString.replaceAll("[\"]", " ");
+		poemString = poemString.replaceAll("[“]", " ");
+		poemString = poemString.replaceAll("[”]", " ");
+		poemString = poemString.replaceAll("[!]", " ");
+		poemString = poemString.replaceAll("[;]", " ");
+		poemString = poemString.replaceAll("[?]", " ");
+		poemString = poemString.replaceAll("  ", " ");
+		poemString = poemString.replaceAll("  ", " ");
+
+		String words[] = poemString.toLowerCase().split(" ");
+		for (int i = 0; i < words.length; i++) {
+			//words[i] = words[i].replaceAll("[^A-Za-z']", " ");
+			System.out.println(words[i]);
+		}
+			
 	}
 	
-	//reads the file and converts it to a string
 	public static String getText() throws Exception {
-		String text = new String();
-		FileReader reader = new FileReader("TheRavenPoemWithHTMLTags.txt");
+		// Parse the HTML text into a Document object
+		File input = new File("TheRavenPoemWithHTMLTags.txt");
+		Document doc = Jsoup.parse(input, "UTF-8");
 		
-		int i;
-		while ((i = reader.read()) != -1) {
-			text += (char)i;
+		// Get the title of the poem
+		Element title = doc.getElementsByTag("h1").first();
+		String titleText = title.text();
+		//System.out.println(titleText);
+		
+		// Get the subtitle of the poem (author name)
+		Element author = doc.getElementsByTag("h2").first();
+		String authorText = author.text();
+		//System.out.println(authorText);
+		
+		// Get each paragraph from the poem
+		String poemText = new String();
+		Elements paragraphs = doc.getElementsByTag("p");
+		for (int i = 0; i < paragraphs.size(); i++) {
+			String paragraph = paragraphs.get(i).text();
+			poemText += paragraph;
 		}
 		
-		reader.close();
-		return text;
+		String finalText = titleText + " " + authorText + " " + poemText;
+		return finalText;
 	}
-
 }
