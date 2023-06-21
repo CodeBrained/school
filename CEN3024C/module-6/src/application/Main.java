9c98f4a3011217ca3975dec8fe0060506d8ee720
CodeBrained <cristianking128@gmail.com>
2023-06-21 03:11:30 -0400

    Progressed to 3 buttons. Created WordParser Class. Button 1 succcessfully prints top 20 words to the console.

--- CEN3024C/module-6/src/application/Main.java
+++ CEN3024C/module-6/src/application/Main.java
@@ -5,14 +5,14 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.control.Button;
+//import javafx.scene.control.Label;
 
 
 
 public class Main extends Application implements EventHandler<ActionEvent> {
-	
-	Button button;
+	Button btn1, btn2, btn3;
 	Stage window;
 	
 	public static void main(String[] args) {
@@ -23,16 +23,40 @@ public class Main extends Application implements EventHandler<ActionEvent> {
 	public void start(Stage window) {
 		try {
 			window.setTitle("Word Parser");
-			button = new Button();
-			button.setText("top 20");
 			
-			button.setOnAction(this);
+			//Button 1
+			btn1 = new Button();
+			btn1.setText("top 20");
+			btn1.setOnAction(this);
+			btn1.setLayoutX(10);
+			btn1.setLayoutY(20);
 			
-			StackPane sp = new StackPane();
-			sp.getChildren().add(button);
+			//Button 2
+			btn2 = new Button();
+			btn2.setText("does word exist");
+			btn2.setOnAction(this);
+			btn2.setLayoutX(10);
+			btn2.setLayoutY(50);
 			
-			Scene scene1, scene2, scene3;
-			scene1 = new Scene(sp, 300, 250);
+			//Button 3
+			btn3 = new Button();
+			btn3.setText("full poem");
+			btn3.setOnAction(this);
+			btn3.setLayoutX(10);
+			btn3.setLayoutY(80);
+			
+			//Pane
+			Pane p = new Pane();
+			p.getChildren().add(btn1);
+			
+			p.getChildren().add(btn2);
+			p.getChildren().add(btn3);
+			//sp.getChildren().add(button2);
+			//sp.getChildren().add(button3);
+			
+			Scene scene1;
+			scene1 = new Scene(p, 300, 250);
+			//scene2 = new Scene(p, 300, 250);
 			
 			window.setScene(scene1);
 			window.show();
@@ -42,8 +66,23 @@ public class Main extends Application implements EventHandler<ActionEvent> {
 	}
 	
 	public void handle(ActionEvent event) {
-		if(event.getSource()==button) {
-															//FIXME
+		if(event.getSource()==btn1) {
+			System.out.println("top 20 words");
+			WordParser wp = new WordParser();
+			try {
+				wp.wordParser();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+		}
+		if(event.getSource()==btn2) {
+			System.out.println("does word exist");
+		}
+		if(event.getSource()==btn3) {
+			System.out.println("full poem");
 		}
 	}
 }
