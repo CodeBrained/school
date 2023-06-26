f6014bed4bc4b25ce4579b656a5311f958bdec1a
CodeBrained <cristianking128@gmail.com>
2023-06-26 02:29:05 -0400

    Added comments to WordParserTest.java

--- CEN3024C/module-7/src/module7/WordParserTest.java
+++ CEN3024C/module-7/src/module7/WordParserTest.java
@@ -44,11 +44,13 @@ class WordParserTest {
 	
 	@Test
 	void countWordFreqTest() throws Exception {
+		// sample unique words list
 		List<String> s1 = new ArrayList<>(3);
 		s1.add("horse");
 		s1.add("pig");
 		s1.add("cow");
 		
+		// sample list with duplicates
 		List<String> s2 = new ArrayList<>(6);
 		s2.add("horse");
 		s2.add("horse");
@@ -58,7 +60,11 @@ class WordParserTest {
 		s2.add("pig");
 		
 		Hashtable<String, Integer> wordMap = WordParser.countWordFreq(s1, s2);
+		// Prints the word frequencies to the console. 
 		System.out.println(wordMap);
+		
+		// I changed the values to incorrect values to assure it wuld produce a failure
+		// The current values will not produce a failure. 
 		if (wordMap.get("horse")!=3) {
 			fail("The method did not count horse as 3");
 		}
@@ -67,10 +73,6 @@ class WordParserTest {
 		}
 		if (wordMap.get("cow")!=2) {
 			fail("The method did not count cow as 2");
-		}
-		//List<Map.Entry<String, Integer>> list = new ArrayList<Entry<String, Integer>>(wordMap.entrySet());
-		
-		
-		
+		}		
 	}
 }
