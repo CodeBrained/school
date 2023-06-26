4881ed2136d83ca92ffb5e287b5cf1b6ac23da5a
CodeBrained <cristianking128@gmail.com>
2023-06-26 01:37:38 -0400

    Finished unit testing for 3 methods

--- CEN3024C/module-7/README.md
+++ CEN3024C/module-7/README.md
@@ -4,6 +4,7 @@ June 2023
 ## Objective
 * Add unit testing to the Module 2 Word Parser class using JUnit.
 
+
 ## Deliverables
 - [] One page explanation explaining the design of the unit tests.
 - [] .java files for the application.
@@ -11,6 +12,29 @@ June 2023
 - [] Submit to GitHub.
 - [] Provide GitHub link.
 
+
 ## Methods Tested
-* `formatList()`
-* `getText()`
+* `uglyRegex()` 
+* `removeApo()`
+* `countWordFreq()`
+
+
+## References
+
+### Youtube
+* [JUnit 5 Basics 7 - Creating a JUnit test](https://www.youtube.com/watch?v=EROuIf2Ac_I)
+* [ Java - JUnit testing in Eclipse ](https://www.youtube.com/watch?v=I8XXfgF9GSc&t=17s)
+* [unit testing | software engineering](https://www.youtube.com/watch?v=c2Gv47azQ2Q)
+
+### Stack Overflow
+* [Remove all occurrences of an element from ArrayList](https://stackoverflow.com/questions/13565876/remove-all-occurrences-of-an-element-from-arraylist)
+
+### Methods
+* [Using Fail Assertion in JUnit: fail()](https://www.baeldung.com/junit-fail)
+* [contains()](https://www.w3schools.com/java/ref_string_contains.asp)
+
+### Class
+* [Hashtable<K,V>](https://docs.oracle.com/javase/8/docs/api/java/util/Hashtable.html)
+
+### Other
+* [Iterate over chars of a String in Java](https://www.geeksforgeeks.org/iterate-over-the-characters-of-a-string-in-java/)
---
+++ CEN3024C/module-7/src/module7/WordParserTest.java
@@ -0,0 +1,76 @@
+package module7;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.junit.jupiter.api.Test;
+
+class WordParserTest {
+
+	@Test
+	void uglyRegexTest() throws Exception {
+		String testString = ("H,ell?o");
+		testString = WordParser.uglyRegex(testString);
+		if (testString.contains(",") == true || testString.contains("?") == true) {
+			fail("The String contains either a ',' or a '?'");
+		}
+	}
+	
+	@Test
+	void removeApoTest() throws Exception {
+		WordParser wp = new WordParser();
+		
+		//Adding another ’ in the string below will result in a failure, proving the test 
+		String raString = new String("Hello World ’ this is a string");
+		String wordsTest[] = raString.toLowerCase().split(" ");
+		List<String> wordList = wp.removeApo(wordsTest);
+		
+		for (int i = 0; i < wordList.size(); i++) {
+			if (wordList.get(i).contains("’")) {
+				fail("The String still contains a '’'");
+			}
+		}
+	}
+	
+	
+	@Test
+	void countWordFreqTest() throws Exception {
+		List<String> s1 = new ArrayList<>(3);
+		s1.add("horse");
+		s1.add("pig");
+		s1.add("cow");
+		
+		List<String> s2 = new ArrayList<>(6);
+		s2.add("horse");
+		s2.add("horse");
+		s2.add("horse");
+		s2.add("cow");
+		s2.add("cow");
+		s2.add("pig");
+		
+		Hashtable<String, Integer> wordMap = WordParser.countWordFreq(s1, s2);
+		System.out.println(wordMap);
+		if (wordMap.get("horse")!=3) {
+			fail("The method did not count horse as 3");
+		}
+		if (wordMap.get("pig")!=1) {
+			fail("The method did not count pig as 1");
+		}
+		if (wordMap.get("cow")!=2) {
+			fail("The method did not count cow as 2");
+		}
+		//List<Map.Entry<String, Integer>> list = new ArrayList<Entry<String, Integer>>(wordMap.entrySet());
+		
+		
+		
+	}
+}
