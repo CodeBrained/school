7e2af30873a17cf2502d5a749f435c0bd8a1ba89
CodeBrained <cristianking128@gmail.com>
2023-09-16 17:15:08 -0400

    More Git testing

---
+++ CEN4025/module2/src/Main.java
@@ -0,0 +1,16 @@
+/*
+Cristian King
+to-do-item application written in Java with the following features:
+- Add a to-do item.
+- Delete a to-do item.
+- View the to-do item.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.println("mod2");
+
+    }
+}
