71e80a864a82e17c7465cddccc85a4fdfd3003b1
CodeBrained <cristianking128@gmail.com>
2023-09-16 18:37:57 -0400

    Fixing git

---
+++ CEN3024C/fileTree/src/fileTree/FileTree.java
@@ -0,0 +1,10 @@
+package fileTree;
+
+public class FileTree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- CEN4025/module2/src/Main.java
+++ CEN4025/module2/src/Main.java
@@ -6,11 +6,32 @@ to-do-item application written in Java with the following features:
 - View the to-do item.
  */
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.println("mod2");
+        System.out.println();
+        System.out.println("Welcome to the to-do list application.");
+        System.out.println("Enter '1' to add an item.");
+        System.out.println("Enter '2' to delete an item.");
+        System.out.println("Enter '3' to view your to-do item list.");
+        System.out.println("Enter '-1' to quit.");
 
+        Scanner scan = new Scanner(System.in);
+        int userChoice = 0;
+        while (userChoice != -1) {
+            userChoice = scan.nextInt();
+            if (userChoice == 1) {
+                // add item
+                System.out.println("add item");
+            } else if (userChoice == 2) {
+                // delete item
+                System.out.println("delete item");
+            } else if (userChoice == 3) {
+                // view list
+                System.out.println("view list");
+            }
+
+        }
     }
 }
