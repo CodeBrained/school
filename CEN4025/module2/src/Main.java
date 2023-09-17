81cabb3f168d0e87f5ab4c504d12c1cd76f6a1f5
CodeBrained <cristianking128@gmail.com>
2023-09-16 20:24:56 -0400

    added the framework to the project

--- CEN4025/module2/src/Main.java
+++ CEN4025/module2/src/Main.java
@@ -7,31 +7,48 @@ to-do-item application written in Java with the following features:
  */
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
-        System.out.println("Welcome to the to-do list application.");
-        System.out.println("Enter '1' to add an item.");
-        System.out.println("Enter '2' to delete an item.");
-        System.out.println("Enter '3' to view your to-do item list.");
-        System.out.println("Enter '-1' to quit.");
-
+        Methods.intro();
+        Methods.continueMessage();
         Scanner scan = new Scanner(System.in);
+        ArrayList<String> items = new ArrayList<String>();
+        items.add("Hello");
+        items.add("World");
+        items.add("Test");
+
         int userChoice = 0;
         while (userChoice != -1) {
-            userChoice = scan.nextInt();
-            if (userChoice == 1) {
-                // add item
-                System.out.println("add item");
-            } else if (userChoice == 2) {
-                // delete item
-                System.out.println("delete item");
-            } else if (userChoice == 3) {
-                // view list
-                System.out.println("view list");
+            userChoice = Integer.parseInt(scan.nextLine());
+            switch (userChoice) {
+                case 1:
+                    System.out.println("\nWrite your to-do note.");
+                    String toDoNote = scan.nextLine();
+                    // FIXME create a way to add notes to ArrayList 'items'
+                    System.out.println("\nYour note has been added.");
+                    Methods.continueMessage();
+                    break;
+                case 2:
+                    System.out.println("Enter the index of the item you wish to delete ");
+                    System.out.print("If you don't know the index, enter -1 to cancel, and ");
+                    System.out.print("then enter 3 to view the to-do list.");
+                    int num = Integer.parseInt(scan.nextLine());
+                    // FIXME create a way to delete item from ArrayList 'items'
+                    System.out.println("\nYour note has been deleted.");
+                    break;
+                case 3:
+                    System.out.println("view list");
+                    for (int i = 0; i < items.size(); i++) {
+                        System.out.println(i + " " + items.get(i));
+                    }
+                    Methods.continueMessage();
+                    break;
+                default:
+                    System.out.println("Out of range");
             }
-
         }
+        scan.close();
     }
 }
