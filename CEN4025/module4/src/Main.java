ed760423113d517d35f20255cc359a412a6661f4
CodeBrained <cristianking128@gmail.com>
2023-09-24 16:54:06 -0400

    Created assignment for module 4

---
+++ CEN4025/module4/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 2000000;
+        arrayListMethod(n);
+    }
+
+    public static void arrayListMethod(int n) {
+        // Creates new ArrayList to hold n amount of random integers
+        ArrayList<Integer> randomArray = new ArrayList<>();
+    }
+
+    public static void linkedListMethod(int n) {
+        // FIXME
+    }
+
+    public static void hashtableMethod(int n) {
+        // FIXME
+    }
+}
